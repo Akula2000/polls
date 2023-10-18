147c9d9b5f13104870a2454c4d6f0b60055d750b
Akula2000 <varshitha.v.akula@gmail.com>
2023-10-17 21:21:18 -0400

    pytest + flake fixes

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 # from django.http import Http404
 from django.shortcuts import get_object_or_404, render
-# from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 
 from django.urls import reverse
 

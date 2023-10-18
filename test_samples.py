cfad84dfc99ff6279c4d0bdb7cc6ba71cf5d635c
Akula2000 <varshitha.v.akula@gmail.com>
2023-10-17 23:41:39 -0400

    testcases

--- test_samples.py
+++ test_samples.py
@@ -1,14 +1,16 @@
-def is_even(n):
-    return n % 2 == 0
+def test_basic(self):
+       self.assertEqual(1, 1)
+# def is_even(n):
+    # return n % 2 == 0
 
 
-def is_odd(n):
-    return n % 2 == 1
+# def is_odd(n):
+    # return n % 2 == 1
 
 
-def test_even():
-    assert is_even(12) is True
+# def test_even():
+    # assert is_even(12) is True
 
 
-def test_odd():
-    assert is_odd(15) is True
+# def test_odd():
+    # assert is_odd(15) is True

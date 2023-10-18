45e8c1e3a86e68650ccdd4369787381525a5c254
Akula2000 <varshitha.v.akula@gmail.com>
2023-10-18 00:01:36 -0400

    black formatted

--- test_samples.py
+++ test_samples.py
@@ -1,11 +1,14 @@
 def is_even(n):
-    return n%2 == 0
+    return n % 2 == 0
+
 
 def is_odd(n):
-    return n%2 == 1
+    return n % 2 == 1
+
 
 def test_even():
-    assert is_even(12) == True
+    assert is_even(12) is True
+
 
 def test_odd():
-    assert is_odd(15) == True
+    assert is_odd(15) is True
